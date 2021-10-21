f6c939ea87b60a803d89ca78fd16e23b9a98f135
TheQuizzer <85128882+TheQuizzer@users.noreply.github.com>
2021-10-21 13:13:40 +0530

    Update index.js

--- handler/index.js
+++ handler/index.js
@@ -41,12 +41,6 @@ module.exports = async (client) => {
         arrayOfSlashCommands.push(file);
     });
     client.on("ready", async () => {
-        // Register for a single guild
-        await client.guilds.cache
-            .get("PUT YOUR MAIN SERVER ID HERE")
-            .commands.set(arrayOfSlashCommands);
-
-        // Register for all the guilds the bot is in
         await client.application.commands.set(arrayOfSlashCommands);
     });
 
